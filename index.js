a1df1c54956f19fae9928c492b38d48d24761c1b
mks-94 <muralikrishna9424.com>
2020-12-30 06:39:28 +0530

    Mongodb server established successfully.

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+//Setup express
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Create a listening port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`The servre started on the port: ${PORT}`));
+
+//Connect mongoose with local mongodb
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/mern-auth",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) throw err;
+    console.log("MongoDB server established successfully!");
+  }
+);
